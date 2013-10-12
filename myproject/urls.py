f61834fd098f04817dd4ba38de0a20e6414e4770
kar288 <kar288@gmail.com>
2013-10-11 20:25:56 -0400

    trying grumblr

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,30 +1,17 @@
 from django.conf.urls import patterns, include, url
-from django.conf import settings
-from django.conf.urls.static import static
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.contrib.auth.views.login', {'template_name':'sign-in.html'}, name='login'),
-    url(r'^login', 'django.contrib.auth.views.login', {'template_name':'sign-in.html'}, name='login'),
-    url(r'^sign-in.html', 'django.contrib.auth.views.login', {'template_name':'sign-in.html'}, name='login'),
-    
-    url(r'^profile$', 'grumblr.views.profile', name='profile'),
-    url(r'^registration$', 'grumblr.views.registration', name='registration'),
-    url(r'^lost-password.html$', 'grumblr.views.lostpassword'),
-    url(r'^recover-password$', 'grumblr.views.recoverpassword', name='recover-password'),
-    url(r'^stream$', 'grumblr.views.stream', name = 'stream'),
-    url(r'^profile/(?P<name>\w+)', 'grumblr.views.other_user', name = 'other_user'),
-    url(r'^edit-information$', 'grumblr.views.edit_information', name ='edit_information'),
-    url(r'^search', 'grumblr.views.search', name = 'search'),
-    url(r'^logout', 'grumblr.views.logout', name = 'logout'),
-    url(r'^edit-password', 'grumblr.views.edit_password', name = 'edit_password'),
-    url(r'^dislike', 'grumblr.views.dislike', name = 'dislike'),
-    url(r'^disdislike', 'grumblr.views.disdislike', name = 'disdislike'),
-    url(r'^photo/(?P<id>\d+)$', 'grumblr.views.get_photo', name='photo'),
-    # The following URL should match any username valid in Django and
-    # any token produced by the default_token_generator
-    url(r'^confirm-registration/(?P<username>[a-zA-Z0-9_@\+\-]+)/(?P<token>[a-z0-9\-]+)$', 'grumblr.views.confirm_registration', name='confirm'),
-    url(r'^needs-confirmation.html$', 'grumblr.views.needs_confirmation'), 
-    url(r'^grumbls_json.html$', 'grumblr.views.grumbls_json', name="grumbls_json"), 
-    
+    # Examples:
+    # url(r'^$', 'myproject.views.home', name='home'),
+    # url(r'^myproject/', include('myproject.foo.urls')),
 
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
 )
